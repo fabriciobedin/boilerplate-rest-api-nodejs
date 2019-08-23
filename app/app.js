809bb890ac54e2cf9581642de696c2cb059c974f
Fabricio <fabricioobedin@gmail.com>
2019-08-22 23:57:57 -0300

    refactoring server, app and controller

--- app/app.js
+++ app/app.js
@@ -1,17 +1,23 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const userRouter = require("./routes/users");
 
-var app = express();
+mongoose
+  .connect(
+    `mongodb+srv://adminmongo:${
+      process.env.MONGO_CLOUD_PW
+    }@cluster0-vzoaz.gcp.mongodb.net/shop?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .catch(err => {
+    console.log("Database connection error!" + err);
+  });
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -19,23 +25,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api/users", userRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 module.exports = app;
